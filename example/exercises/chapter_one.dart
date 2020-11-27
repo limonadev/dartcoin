a7d3ef5a2991cd04ff3ada9f059a1c522ef008ab
limonadev <limonadev@whiteleaf.dev>
2020-11-27 15:14:13 -0500

    Added add/sub exercises from chapter 1

--- example/exercises/chapter_one.dart
+++ example/exercises/chapter_one.dart
@@ -4,13 +4,24 @@ class ChapterOne {
   void runEverything() {
     print('Run each exercise from Chapter One');
     _first();
+    _second();
   }
 
   void _first() {
+    print('First Exercise');
     var a = FieldElement(number: 7, prime: 13);
     var b = FieldElement(number: 6, prime: 13);
 
     print(a == b);
     print(a == a);
+  }
+
+  void _second() {
+    print('Second Exercise');
+    var a = FieldElement(number: 7, prime: 13);
+    var b = FieldElement(number: 12, prime: 13);
+    var c = FieldElement(number: 6, prime: 13);
+
+    print(a + b == c);
   }
 }
