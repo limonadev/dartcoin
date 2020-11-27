1f748e479a073b8ae67dc0b06d5e1fa59ade81a8
limonadev <limonadev@whiteleaf.dev>
2020-11-27 15:06:58 -0500

    Clear default TODO messages

--- lib/dartcoin.dart
+++ lib/dartcoin.dart
@@ -5,7 +5,5 @@ library dartcoin;
 
 export 'src/dartcoin_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
-
 export 'src/finite_field/field_element.dart';
 export 'src/utils/all.dart';
