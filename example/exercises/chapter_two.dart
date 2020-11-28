2c29ce89d77f01e405183683f7d27bd19713b63d
limonadev <limonadev@whiteleaf.dev>
2020-11-28 12:38:18 -0500

    Added first addition exercises from chapter 2

--- example/exercises/chapter_two.dart
+++ example/exercises/chapter_two.dart
@@ -4,6 +4,7 @@ class ChapterTwo {
   void runEverything({bool runOptionals = false}) {
     print('Run each exercise from Chapter Two');
     _first();
+    _second();
 
     if (runOptionals) {
       print('Optional exercises');
@@ -19,5 +20,15 @@ class ChapterTwo {
     } catch (e) {
       print('EXPECTED FAILED ASSERTION:\n$e');
     }
+  }
+
+  /// Exercise 2 from https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch02/Chapter2.ipynb
+  void _second() {
+    var p1 = Point(a: 5, b: 7, x: -1, y: -1);
+    var p2 = Point(a: 5, b: 7, x: -1, y: 1);
+    var inf = Point.atInfinity(a: 5, b: 7);
+    print(p1 + inf);
+    print(inf + p2);
+    print(p1 + p2);
   }
 }
