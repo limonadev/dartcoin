3313142bc32352383613e7bdf375fbc49c6271ab
limonadev <limonadev@whiteleaf.dev>
2020-12-01 15:04:03 -0500

    Added s256 FieldElement

---
+++ lib/src/finite_field/s256_field_element.dart
@@ -0,0 +1,16 @@
+import 'package:dartcoin/src/models/operand.dart';
+import 'package:dartcoin/src/utils/all.dart';
+import 'package:meta/meta.dart';
+import 'package:dartcoin/src/finite_field/field_element.dart';
+
+class S256FieldElement extends FieldElement {
+  S256FieldElement({@required BigInt value})
+      : super(
+          prime: Secp256Utils.prime,
+          value: value,
+        );
+
+  factory S256FieldElement.infinity() => S256FieldElement(
+        value: Operand.INF,
+      );
+}
