991defa6603242ae76bfb2380d6458c18c87f228
limonadev <limonadev@whiteleaf.dev>
2020-12-04 13:55:20 -0500

    Added bigIntToBytes tests

--- test/utils_test/object_utils_test.dart
+++ test/utils_test/object_utils_test.dart
@@ -88,4 +88,104 @@ void main() {
       ),
     );
   });
+
+  group('BigInt to bytes', () {
+    test('Value lower than 32 bytes', () {
+      final value = BigInt.one;
+      expect(
+        ObjectUtils.encodeBigInt(value).length < 32,
+        equals(true),
+      );
+
+      var size = 10;
+      var encoded = ObjectUtils.bigIntToBytes(
+        number: value,
+        size: size,
+      );
+      expect(
+        encoded,
+        equals([...List.filled(size - 1, 0), 1]),
+      );
+
+      size = 32;
+      encoded = ObjectUtils.bigIntToBytes(
+        number: value,
+        size: size,
+      );
+      expect(
+        encoded,
+        equals([...List.filled(size - 1, 0), 1]),
+      );
+    });
+
+    test('Value equal to 32 bytes', () {
+      var value = BigInt.parse(
+        '8995fa52fa91ee28d116a75756397692488901dcce7352b347fa859ee35d15d9',
+        radix: 16,
+      );
+      expect(
+        ObjectUtils.encodeBigInt(value).length,
+        equals(32),
+      );
+
+      var size = 32;
+      var encoded = ObjectUtils.bigIntToBytes(
+        number: value,
+        size: size,
+      );
+      expect(
+        encoded,
+        equals([
+          ...[137, 149, 250, 82, 250, 145, 238, 40],
+          ...[209, 22, 167, 87, 86, 57, 118, 146],
+          ...[72, 137, 1, 220, 206, 115, 82, 179],
+          ...[71, 250, 133, 158, 227, 93, 21, 217]
+        ]),
+      );
+
+      size = 10;
+      expect(
+        () => ObjectUtils.bigIntToBytes(
+          number: value,
+          size: size,
+        ),
+        throwsRangeError,
+      );
+    });
+
+    test('Value greater than 32 bytes', () {
+      var value = BigInt.parse(
+        '8995fa52fa91ee28d116a75756397692488901dcce7352b347fa859ee35d15d9ffffffff',
+        radix: 16,
+      );
+      expect(
+        ObjectUtils.encodeBigInt(value).length,
+        equals(36),
+      );
+
+      var size = 36;
+      var encoded = ObjectUtils.bigIntToBytes(
+        number: value,
+        size: size,
+      );
+      expect(
+        encoded,
+        equals([
+          ...[137, 149, 250, 82, 250, 145, 238, 40, 209],
+          ...[22, 167, 87, 86, 57, 118, 146, 72, 137],
+          ...[1, 220, 206, 115, 82, 179, 71, 250, 133],
+          ...[158, 227, 93, 21, 217, 255, 255, 255, 255],
+        ]),
+      );
+
+      size = 32;
+      expect(
+        () => ObjectUtils.bigIntToBytes(
+          number: value,
+          size: size,
+        ),
+        throwsRangeError,
+      );
+    });
+  });
 }
