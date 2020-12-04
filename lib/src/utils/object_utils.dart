90efc9721d7147fd9f552b13ca2aa61670e60cc3
limonadev <limonadev@whiteleaf.dev>
2020-12-04 12:36:33 -0500

    Moved methods od pointycastle to avoid import utils

--- lib/src/utils/object_utils.dart
+++ lib/src/utils/object_utils.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
 class ObjectUtils {
+  /// Took from https://github.com/bcgit/pc-dart/blob/master/lib/src/utils.dart
+  static final _byteMask = BigInt.from(0xff);
+
   /// Implementation based on Effective Java Book, Item 11
   static int buildHashCode(List<Object> objects) {
     var result = 17;
@@ -8,6 +13,27 @@ class ObjectUtils {
       result = 31 * result + objectHash;
     });
 
+    return result;
+  }
+
+  /// Took from https://github.com/bcgit/pc-dart/blob/master/lib/src/utils.dart
+  static BigInt decodeBigInt(List<int> bytes) {
+    var result = BigInt.from(0);
+    for (var i = 0; i < bytes.length; i++) {
+      result += BigInt.from(bytes[bytes.length - i - 1]) << (8 * i);
+    }
+    return result;
+  }
+
+  /// Took from https://github.com/bcgit/pc-dart/blob/master/lib/src/utils.dart
+  static Uint8List encodeBigInt(BigInt number) {
+    // Not handling negative numbers. Decide how you want to do that.
+    var size = (number.bitLength + 7) >> 3;
+    var result = Uint8List(size);
+    for (var i = 0; i < size; i++) {
+      result[size - i - 1] = (number & _byteMask).toInt();
+      number = number >> 8;
+    }
     return result;
   }
 
