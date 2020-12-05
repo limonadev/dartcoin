fbd775c8a7bc2ee5db262f5715808bed040f9d9b
limonadev <limonadev@whiteleaf.dev>
2020-12-05 12:46:37 -0500

    Added serialization tests

--- test/elliptic_curve_cryptography_test.dart/s256_point_test.dart
+++ test/elliptic_curve_cryptography_test.dart/s256_point_test.dart
@@ -125,4 +125,81 @@ void main() {
       equals(true),
     );
   });
+
+  test('test_sec', () {
+    var coefficient = BigInt.from(999).pow(3);
+    var point = Secp256Utils.generator * coefficient;
+    var uncompressed = BigInt.parse(
+      '049d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d56fa15cc7f3d38cda98dee2419f415b7513dde1301f8643cd9245aea7f3f911f9',
+      radix: 16,
+    );
+    var compressed = BigInt.parse(
+      '039d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d5',
+      radix: 16,
+    );
+    expect(
+      point.sec(compressed: false),
+      ObjectUtils.bigIntToBytes(
+        number: uncompressed,
+        size: 65,
+      ),
+    );
+    expect(
+      point.sec(compressed: true),
+      ObjectUtils.bigIntToBytes(
+        number: compressed,
+        size: 33,
+      ),
+    );
+
+    coefficient = BigInt.from(123);
+    point = Secp256Utils.generator * coefficient;
+    uncompressed = BigInt.parse(
+      '04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b',
+      radix: 16,
+    );
+    compressed = BigInt.parse(
+      '03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5',
+      radix: 16,
+    );
+    expect(
+      point.sec(compressed: false),
+      ObjectUtils.bigIntToBytes(
+        number: uncompressed,
+        size: 65,
+      ),
+    );
+    expect(
+      point.sec(compressed: true),
+      ObjectUtils.bigIntToBytes(
+        number: compressed,
+        size: 33,
+      ),
+    );
+
+    coefficient = BigInt.from(42424242);
+    point = Secp256Utils.generator * coefficient;
+    uncompressed = BigInt.parse(
+      '04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3',
+      radix: 16,
+    );
+    compressed = BigInt.parse(
+      '03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e',
+      radix: 16,
+    );
+    expect(
+      point.sec(compressed: false),
+      ObjectUtils.bigIntToBytes(
+        number: uncompressed,
+        size: 65,
+      ),
+    );
+    expect(
+      point.sec(compressed: true),
+      ObjectUtils.bigIntToBytes(
+        number: compressed,
+        size: 33,
+      ),
+    );
+  });
 }
