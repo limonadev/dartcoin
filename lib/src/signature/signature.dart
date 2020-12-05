8b4d19132e2d9c8ecbe96e25496aff4727ef5ffc
limonadev <limonadev@whiteleaf.dev>
2020-12-05 13:55:18 -0500

    Added Signature DER serialization

--- lib/src/signature/signature.dart
+++ lib/src/signature/signature.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:dartcoin/src/utils/all.dart';
 import 'package:meta/meta.dart';
 
 //TODO: Migrate to Dart latest SDK for null safety
@@ -10,6 +13,25 @@ class Signature {
 
   final BigInt r;
   final BigInt s;
+
+  Uint8List der() {
+    var encoded = <int>[];
+
+    List<int> rEncoded = ObjectUtils.bigIntToBytes(number: r);
+    if (rEncoded[0] & 128 == 128) {
+      rEncoded = [0x00, ...rEncoded];
+    }
+    encoded += [0x02, rEncoded.length] + rEncoded;
+
+    List<int> sEncoded = ObjectUtils.bigIntToBytes(number: s);
+    if (sEncoded[0] & 128 == 128) {
+      sEncoded = [0x00, ...sEncoded];
+    }
+    encoded += [0x02, sEncoded.length] + sEncoded;
+
+    encoded = [0x30, encoded.length, ...encoded];
+    return Uint8List.fromList(encoded);
+  }
 
   @override
   String toString() {
