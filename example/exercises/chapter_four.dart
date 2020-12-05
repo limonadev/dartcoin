639e3459042fd18d8efee57cd84967d5109272c4
limonadev <limonadev@whiteleaf.dev>
2020-12-05 12:19:18 -0500

    Added second exercise from chapter 4

--- example/exercises/chapter_four.dart
+++ example/exercises/chapter_four.dart
@@ -5,6 +5,8 @@ class ChapterFour {
     print('Run each exercise from Chapter Fourth');
 
     _first();
+    _second();
+
     if (runOptionals) {
       print('Optional exercises');
     }
@@ -25,6 +27,26 @@ class ChapterFour {
       final sec = pk.point.sec(compressed: false);
       print(ObjectUtils.toHex(
         padding: 130,
+        value: ObjectUtils.decodeBigInt(sec),
+      ));
+    }
+  }
+
+  /// Exercise 2 from https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch04/Chapter4.ipynb
+  void _second() {
+    print('Second Exercise');
+
+    final secrets = [
+      BigInt.from(5001),
+      BigInt.from(2019).pow(5),
+      BigInt.parse('deadbeef54321', radix: 16),
+    ];
+
+    for (var secret in secrets) {
+      final pk = PrivateKey(secret: secret);
+      final sec = pk.point.sec(compressed: true);
+      print(ObjectUtils.toHex(
+        padding: 66,
         value: ObjectUtils.decodeBigInt(sec),
       ));
     }
