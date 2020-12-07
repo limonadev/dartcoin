2582f5d5ac15bab2887648c904b68a2cae7b0bc5
limonadev <limonadev@whiteleaf.dev>
2020-12-07 14:55:10 -0500

    Change from String to Uint8List on base58 encode

--- lib/src/utils/encoding_utils.dart
+++ lib/src/utils/encoding_utils.dart
@@ -2,24 +2,36 @@ import 'dart:typed_data';
 import 'package:meta/meta.dart';
 import 'package:dartcoin/src/utils/all.dart';
 
+// TODO: CHECK THE BASE58 ENCODING, IS IT REALLY NECESSARY TO USE BYTES?
 class Base58Utils {
   static const alphabet =
       '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
-  static String encode({@required Uint8List bytes}) {
+  static Uint8List encode({@required Uint8List bytes}) {
     final zerosCount = bytes.takeWhile((value) => value == 0x00).length;
-    final prefix = List.filled(zerosCount, 1).join();
+    final prefix = List.filled(zerosCount, '1'.codeUnitAt(0));
 
     var decoded = ObjectUtils.decodeBigInt(bytes);
-    var result = '';
+    final result = <int>[];
 
     while (decoded > BigInt.zero) {
       final mod = (decoded % BigInt.from(58)).toInt();
       decoded = decoded ~/ BigInt.from(58);
 
-      result = alphabet[mod] + result;
+      result.insert(0, alphabet[mod].codeUnitAt(0));
     }
 
-    return prefix + result;
+    return Uint8List.fromList(prefix + result);
+  }
+
+  static Uint8List encodeChecksum({@required Uint8List bytes}) {
+    return encode(
+      bytes: Uint8List.fromList(
+        [
+          ...bytes,
+          ...Secp256Utils.hash256(data: bytes).getRange(0, 4).toList()
+        ],
+      ),
+    );
   }
 }
