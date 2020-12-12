d3ddc4414116564675478c1be49053a4f9795713
limonadev <limonadev@whiteleaf.dev>
2020-12-11 19:26:18 -0500

    Updated Script to use an Objects list

--- lib/src/transaction/script.dart
+++ lib/src/transaction/script.dart
@@ -1,12 +1,29 @@
 import 'dart:typed_data';
 
+import 'package:dartcoin/src/utils/all.dart';
 import 'package:meta/meta.dart';
 
-//TODO: MAKE THIS CLASS (CHAPTER 6)
+/// TODO: MAKE THIS CLASS (CHAPTER 6)
 class Script {
   Script({
-    @required List<Uint8List> cmds,
-  }) : cmds = cmds ?? [Uint8List.fromList([])];
+    @required List<Object> cmds,
+  }) : cmds = cmds ?? [];
 
-  final List<Uint8List> cmds;
+  final List<Object> cmds;
+
+  /// TODO: COMPLETE THE REAL STRING REPRESENTATION
+  @override
+  String toString() {
+    final result = <String>[];
+
+    for (final cmd in cmds) {
+      if (cmd is int) {
+        /// TODO: TOSTRING WHEN OPCODE
+      } else if (cmd is Uint8List) {
+        final value = ObjectUtils.bytesToBigInt(bytes: cmd);
+        result.add(ObjectUtils.toHex(value: value));
+      }
+    }
+    return result.join(' ');
+  }
 }
