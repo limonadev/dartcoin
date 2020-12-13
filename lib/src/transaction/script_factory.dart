9cd71f81a810bc171c9feff3ef68d25b704a3ff0
limonadev <limonadev@whiteleaf.dev>
2020-12-13 14:00:24 -0500

    Update Script parseSync to allow initialIndex

--- lib/src/transaction/script_factory.dart
+++ lib/src/transaction/script_factory.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:dartcoin/src/transaction/script.dart';
 import 'package:dartcoin/src/transaction/varint.dart';
 import 'package:dartcoin/src/utils/all.dart';
+import 'package:meta/meta.dart';
 
 /// TODO: UNDERSTAND ON CHAPTER 6 BUILD SCRIPT FROM STREAM
 enum ScriptParsing {
@@ -14,56 +15,58 @@ enum ScriptParsing {
 }
 
 class ScriptFactory {
-  static Script parseSync(Uint8List bytes) {
+  static ParsingResult<Script> parseSync({
+    @required Uint8List bytes,
+    int initialIndex = 0,
+  }) {
+    final realBytes = bytes.sublist(initialIndex);
+
     final cmds = <Object>[];
 
     final lengthVarintByteNumber = Varint.numberOfNecessaryBytes(
-      flag: bytes[0],
+      flag: realBytes[0],
     );
-    final lengthVarint = Varint.read(bytes: bytes);
-
-    if (BigInt.from(bytes.length - lengthVarintByteNumber - 1) !=
-        lengthVarint) {
-      throw FormatException('ScriptParsing Script failed!');
-    }
+    final lengthVarint = Varint.read(bytes: realBytes);
+    final lenAsInt = lengthVarint.toInt();
 
     int necessaryBytes;
+    var i = lengthVarintByteNumber + 1;
 
-    for (var i = lengthVarintByteNumber + 1; i < bytes.length; i++) {
-      final byte = bytes[i];
+    for (; i < lenAsInt && i < realBytes.length; i++) {
+      final byte = realBytes[i];
 
       if (byte >= 1 && byte <= 75) {
         necessaryBytes = byte;
 
         cmds.add(
           Uint8List.fromList(
-            bytes.sublist(i + 1, i + necessaryBytes + 1),
+            realBytes.sublist(i + 1, i + necessaryBytes + 1),
           ),
         );
 
         i += necessaryBytes;
       } else if (byte == 76) {
         necessaryBytes = ObjectUtils.bytesToBigInt(
-          bytes: Uint8List.fromList([bytes[i + 1]]),
+          bytes: Uint8List.fromList([realBytes[i + 1]]),
           endian: Endian.little,
         ).toInt();
 
         cmds.add(
           Uint8List.fromList(
-            bytes.sublist(i + 2, i + necessaryBytes + 2),
+            realBytes.sublist(i + 2, i + necessaryBytes + 2),
           ),
         );
 
         i += necessaryBytes + 1;
       } else if (byte == 77) {
         necessaryBytes = ObjectUtils.bytesToBigInt(
-          bytes: Uint8List.fromList([bytes[i + 1], bytes[i + 2]]),
+          bytes: Uint8List.fromList([realBytes[i + 1], realBytes[i + 2]]),
           endian: Endian.little,
         ).toInt();
 
         cmds.add(
           Uint8List.fromList(
-            bytes.sublist(i + 3, i + necessaryBytes + 3),
+            realBytes.sublist(i + 3, i + necessaryBytes + 3),
           ),
         );
 
@@ -73,7 +76,14 @@ class ScriptFactory {
       }
     }
 
-    return Script(cmds: cmds);
+    if (BigInt.from(i - lengthVarintByteNumber - 1) != lengthVarint) {
+      throw FormatException('ScriptParsing Script failed!');
+    }
+
+    return ParsingResult(
+      currentBytePosition: initialIndex + i,
+      result: Script(cmds: cmds),
+    );
   }
 
   static Future<Script> parse(Stream<int> stream) async {
