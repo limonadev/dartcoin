b7850be42caf50e20eada9bf2f8b82c3fe284572
limonadev <limonadev@whiteleaf.dev>
2020-12-13 14:20:51 -0500

    Added Script serialization

--- lib/src/transaction/script.dart
+++ lib/src/transaction/script.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:dartcoin/src/transaction/varint.dart';
 import 'package:dartcoin/src/utils/all.dart';
 import 'package:meta/meta.dart';
 
@@ -10,6 +11,54 @@ class Script {
   }) : cmds = cmds ?? [];
 
   final List<Object> cmds;
+
+  Uint8List rawSerialize() {
+    final result = <int>[];
+
+    for (final cmd in cmds) {
+      if (cmd is int) {
+        result.add(cmd);
+      } else if (cmd is List<int>) {
+        final len = cmd.length;
+
+        if (len < 75) {
+          result.add(len);
+        } else if (len > 75 && len < 0x100) {
+          result.addAll([76, len]);
+        } else if (len >= 0x100 && len <= 520) {
+          result.addAll(
+            [
+              77,
+              ...ObjectUtils.bigIntToBytes(
+                number: BigInt.from(len),
+                endian: Endian.little,
+                size: 2,
+              )
+            ],
+          );
+        } else {
+          throw ArgumentError('The cmd is too long!');
+        }
+
+        result.addAll(cmd);
+      }
+    }
+
+    return Uint8List.fromList(result);
+  }
+
+  Uint8List serialize() {
+    final result = rawSerialize();
+    final total = result.length;
+    return Uint8List.fromList(
+      [
+        ...Varint.encode(
+          varint: BigInt.from(total),
+        ),
+        ...result,
+      ],
+    );
+  }
 
   /// TODO: COMPLETE THE REAL STRING REPRESENTATION
   @override
