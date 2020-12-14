a3b1a8768474c6c6fbba33060f6818be46b98ad3
limonadev <limonadev@whiteleaf.dev>
2020-12-14 13:31:01 -0500

    Added TxOutputFactory

--- lib/src/transaction/all.dart
+++ lib/src/transaction/all.dart
@@ -4,4 +4,6 @@ export 'transaction.dart';
 export 'transaction_factory.dart';
 export 'transaction_input.dart';
 export 'transaction_input_factory.dart';
+export 'transaction_output.dart';
+export 'transaction_output_factory.dart';
 export 'varint.dart';
---
+++ lib/src/transaction/transaction_output_factory.dart
@@ -0,0 +1,39 @@
+import 'dart:typed_data';
+
+import 'package:dartcoin/src/transaction/script_factory.dart';
+import 'package:dartcoin/src/transaction/transaction_output.dart';
+import 'package:dartcoin/src/utils/all.dart';
+import 'package:meta/meta.dart';
+
+class TxOutputFactory {
+  static ParsingResult<TxOutput> parseSync({
+    @required Uint8List bytes,
+    int initialIndex = 0,
+  }) {
+    final amountBytes = bytes.sublist(
+      initialIndex,
+      initialIndex + 8,
+    );
+    final amount = ObjectUtils.bytesToBigInt(
+      bytes: amountBytes,
+      endian: Endian.little,
+    );
+    initialIndex += 8;
+
+    final scriptPubKeyResult = ScriptFactory.parseSync(
+      bytes: bytes,
+      initialIndex: initialIndex,
+    );
+    initialIndex = scriptPubKeyResult.currentBytePosition;
+    final scriptPubKey = scriptPubKeyResult.result;
+
+    final txOutput = TxOutput(
+      amount: amount,
+      scriptPubkey: scriptPubKey,
+    );
+    return ParsingResult(
+      currentBytePosition: initialIndex,
+      result: txOutput,
+    );
+  }
+}
