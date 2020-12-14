a07a8a18d863ebc52157a1e1167c7d8d38995629
limonadev <limonadev@whiteleaf.dev>
2020-12-14 13:33:01 -0500

    Added TxOutput parsing tests

--- test/transaction_test/transaction_test.dart
+++ test/transaction_test/transaction_test.dart
@@ -62,4 +62,52 @@ void main() {
       ),
     );
   });
+
+  test('test_parse_outputs', () {
+    final rawTx = ObjectUtils.bigIntToBytes(
+      number: BigInt.parse(
+        '0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600',
+        radix: 16,
+      ),
+    );
+    final tx = TransactionFactory.parseSync(bytes: rawTx);
+    expect(
+      tx.txOuts.length,
+      equals(2),
+    );
+
+    var want = BigInt.from(32454049);
+    expect(
+      tx.txOuts[0].amount,
+      equals(want),
+    );
+
+    var wantBytes = ObjectUtils.bigIntToBytes(
+      number: BigInt.parse(
+        '1976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac',
+        radix: 16,
+      ),
+    );
+    expect(
+      tx.txOuts[0].scriptPubkey.serialize(),
+      equals(wantBytes),
+    );
+
+    want = BigInt.from(10011545);
+    expect(
+      tx.txOuts[1].amount,
+      equals(want),
+    );
+
+    wantBytes = ObjectUtils.bigIntToBytes(
+      number: BigInt.parse(
+        '1976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac',
+        radix: 16,
+      ),
+    );
+    expect(
+      tx.txOuts[1].scriptPubkey.serialize(),
+      equals(wantBytes),
+    );
+  });
 }
