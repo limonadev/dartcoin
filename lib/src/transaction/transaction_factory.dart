cf2aa851f3a6d1fdd2112f280555b72f91e4cd3c
limonadev <limonadev@whiteleaf.dev>
2020-12-14 13:32:52 -0500

    Added TxOutput parsing on Transaction parsing

--- lib/src/transaction/transaction_factory.dart
+++ lib/src/transaction/transaction_factory.dart
@@ -3,6 +3,8 @@ import 'dart:typed_data';
 import 'package:dartcoin/src/transaction/transaction.dart';
 import 'package:dartcoin/src/transaction/transaction_input.dart';
 import 'package:dartcoin/src/transaction/transaction_input_factory.dart';
+import 'package:dartcoin/src/transaction/transaction_output.dart';
+import 'package:dartcoin/src/transaction/transaction_output_factory.dart';
 import 'package:dartcoin/src/transaction/varint.dart';
 import 'package:dartcoin/src/utils/all.dart';
 import 'package:meta/meta.dart';
@@ -45,11 +47,29 @@ class TransactionFactory {
       txIns.add(txInResult.result);
     }
 
+    final numberOfTxOutputsResult = _parseNumberOfTxOutputs(
+      bytes: bytes,
+      initialIndex: initialIndex,
+    );
+    initialIndex = numberOfTxOutputsResult.currentBytePosition;
+    final numberOfTxOutputs = numberOfTxOutputsResult.result;
+
+    final txOuts = <TxOutput>[];
+    for (var i = 0; i < numberOfTxOutputs.toInt(); i++) {
+      final txOutResult = TxOutputFactory.parseSync(
+        bytes: bytes,
+        initialIndex: initialIndex,
+      );
+      initialIndex = txOutResult.currentBytePosition;
+
+      txOuts.add(txOutResult.result);
+    }
+
     return Transaction(
       locktime: null,
       testnet: null,
       txIns: txIns,
-      txOuts: null,
+      txOuts: txOuts,
       version: version,
     );
   }
@@ -85,6 +105,25 @@ class TransactionFactory {
     return ParsingResult(
       currentBytePosition: initialIndex + numberOfBytes + 1,
       result: numberOfTxInputs,
+    );
+  }
+
+  static ParsingResult<BigInt> _parseNumberOfTxOutputs({
+    @required Uint8List bytes,
+    @required int initialIndex,
+  }) {
+    final numberOfBytes = Varint.numberOfNecessaryBytes(
+      flag: bytes[initialIndex],
+    );
+    final numberOfTxOutputs = Varint.read(
+      bytes: Uint8List.fromList(
+        bytes.sublist(initialIndex),
+      ),
+    );
+
+    return ParsingResult(
+      currentBytePosition: initialIndex + numberOfBytes + 1,
+      result: numberOfTxOutputs,
     );
   }
 
