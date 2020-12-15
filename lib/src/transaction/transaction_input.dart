8a60875792de1a0b147d4776889499ba3fc5058f
limonadev <limonadev@whiteleaf.dev>
2020-12-15 12:27:21 -0500

    Added TxInput serialize

--- lib/src/transaction/transaction_input.dart
+++ lib/src/transaction/transaction_input.dart
@@ -17,10 +17,31 @@ class TxInput {
               radix: 16,
             );
 
+  /// The [prevTxId] is stored as a list of bytes, when is necessary to
+  /// decode it ensure setting [Endian.little] as part of the process.
   final Uint8List prevTxId;
   final BigInt prevTxIndex;
   final Script scriptSig;
   final BigInt sequence;
+
+  Uint8List serialize() {
+    final result = <int>[
+      ...prevTxId,
+      ...ObjectUtils.bigIntToBytes(
+        number: prevTxIndex,
+        endian: Endian.little,
+        size: 4,
+      ),
+      ...scriptSig.serialize(),
+      ...ObjectUtils.bigIntToBytes(
+        number: sequence,
+        endian: Endian.little,
+        size: 4,
+      ),
+    ];
+
+    return Uint8List.fromList(result);
+  }
 
   @override
   String toString() {
