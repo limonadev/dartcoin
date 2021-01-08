4462409802a2409843f54aa217e6e9cf751b9a2a
limonadev <limonadev@gmail.com>
2021-01-08 12:11:00 -0500

    Added ScriptUtils decodeNumber method

--- lib/src/utils/script_utils.dart
+++ lib/src/utils/script_utils.dart
@@ -27,4 +27,31 @@ class ScriptUtils {
 
     return Uint8List.fromList(result);
   }
+
+  /// Based on https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch06/op.py#L36
+  static BigInt decodeNumber({@required Uint8List element}) {
+    var result = BigInt.zero;
+
+    bool isNegative;
+    if (element.isNotEmpty) {
+      final bigEndian = element.reversed.toList();
+
+      if (bigEndian.first & 0x80 != 0) {
+        result = BigInt.from(bigEndian.first & 0x7f);
+        isNegative = true;
+      } else {
+        result = BigInt.from(bigEndian.first);
+        isNegative = false;
+      }
+
+      for (var byte in element.skip(1)) {
+        result <<= 8;
+        result += BigInt.from(byte);
+      }
+
+      if (isNegative) result = -result;
+    }
+
+    return result;
+  }
 }
