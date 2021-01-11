cdc109a72f95be9e2698e4d364bdf57550b45884
limonadev <limonadev@gmail.com>
2021-01-11 08:36:22 -0500

    Added ScriptUtils method to compare script elements

--- lib/src/utils/script_utils.dart
+++ lib/src/utils/script_utils.dart
@@ -3,29 +3,24 @@ import 'dart:typed_data';
 import 'package:meta/meta.dart';
 
 class ScriptUtils {
-  /// Based on https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch06/op.py#L17
-  static Uint8List encodeNumber({@required BigInt number}) {
-    final result = <int>[];
+  /// Returns if two elements of a Script stack are equal.
+  static bool areStackElementsEqual({
+    @required Uint8List first,
+    @required Uint8List second,
+  }) {
+    var areEqual = false;
 
-    if (number != BigInt.zero) {
-      var abs = number.abs();
-      final isNegative = number < BigInt.zero;
-
-      while (abs != BigInt.zero) {
-        result.add(
-          (abs & BigInt.from(0xff)).toInt(),
-        );
-        abs >>= 8;
-      }
-
-      if (result.last & 0x80 != 0) {
-        result.add(isNegative ? 0x80 : 0);
-      } else if (isNegative) {
-        result[result.length - 1] |= 0x80;
+    if (first.length == second.length) {
+      areEqual = true;
+      for (var i = 0; i < first.length; i++) {
+        if (first[i] != second[i]) {
+          areEqual = false;
+          break;
+        }
       }
     }
 
-    return Uint8List.fromList(result);
+    return areEqual;
   }
 
   /// Based on https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch06/op.py#L36
@@ -53,5 +48,30 @@ class ScriptUtils {
     }
 
     return result;
+  }
+
+  /// Based on https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch06/op.py#L17
+  static Uint8List encodeNumber({@required BigInt number}) {
+    final result = <int>[];
+
+    if (number != BigInt.zero) {
+      var abs = number.abs();
+      final isNegative = number < BigInt.zero;
+
+      while (abs != BigInt.zero) {
+        result.add(
+          (abs & BigInt.from(0xff)).toInt(),
+        );
+        abs >>= 8;
+      }
+
+      if (result.last & 0x80 != 0) {
+        result.add(isNegative ? 0x80 : 0);
+      } else if (isNegative) {
+        result[result.length - 1] |= 0x80;
+      }
+    }
+
+    return Uint8List.fromList(result);
   }
 }
