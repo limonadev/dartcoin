754b7d9fd80b083bab01781456b29214a91f7414
limonadev <limonadev@gmail.com>
2021-01-18 06:48:15 -0500

    Added exercise 2 from Chapter 7

--- example/exercises/chapter_seven.dart
+++ example/exercises/chapter_seven.dart
@@ -5,6 +5,7 @@ class ChapterSeven {
     print('Run each exercise from Chapter Seven');
 
     await _first();
+    _second();
 
     if (runOptionals) {
       print('Optional exercises');
@@ -26,5 +27,30 @@ class ChapterSeven {
     final fee = await tx.getFee();
 
     print(fee >= BigInt.zero);
+  }
+
+  /// Exercise 2 from https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch07/Chapter7.ipynb
+  void _second() {
+    print('Second Exercise');
+
+    final sec = ObjectUtils.bytesFromHex(
+      hex: '0349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a',
+    );
+    final der = ObjectUtils.bytesFromHex(
+      hex:
+          '3045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed',
+    );
+    final z = BigInt.parse(
+      '27e0c5994dec7824e56dec6b2fcb342eb7cdb0d0957c2fce9882f715e85d81a6',
+      radix: 16,
+    );
+    final point = S256Point.fromSerialized(sec: sec);
+    final signature = Signature.fromSerialized(der: der);
+    print(
+      point.verify(
+        sig: signature,
+        z: z,
+      ),
+    );
   }
 }
