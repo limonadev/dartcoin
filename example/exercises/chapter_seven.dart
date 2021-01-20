accb0883155b4d0f4eb14e32088fa9d8b233a13a
limonadev <limonadev@gmail.com>
2021-01-20 09:35:29 -0500

    Added fifth optional exercise from Chapter 7

--- example/exercises/chapter_seven.dart
+++ example/exercises/chapter_seven.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:dartcoin/dartcoin.dart';
 
 class ChapterSeven {
@@ -9,7 +11,9 @@ class ChapterSeven {
       await _optionalFirst();
       _optionalSecond();
       _optionalThird();
-      _optionalFourth();
+      print('Optional Fourth Exercise');
+      print(_optionalFourth());
+      await _optionalFifth();
     }
   }
 
@@ -71,9 +75,7 @@ class ChapterSeven {
   }
 
   /// Exercise 2.1 from https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch07/Chapter7.ipynb
-  void _optionalFourth() {
-    print('Optional Fourth Exercise');
-
+  Transaction _optionalFourth() {
     final prevTxId = ObjectUtils.bytesFromHex(
       hex: '0d6fe5213c0b3291f208cba8bfb59b7476dffacc4e5cb66f6eb20a080843a299',
       endian: Endian.little,
@@ -115,6 +117,73 @@ class ChapterSeven {
       version: BigInt.one,
       locktime: BigInt.zero,
     );
-    print(transaction);
+
+    return transaction;
+  }
+
+  /// Exercise 2.2 from https://github.com/jimmysong/programmingbitcoin/blob/master/code-ch07/Chapter7.ipynb
+  void _optionalFifth() async {
+    print('Optional Fifth Exercise');
+
+    var transaction = _optionalFourth();
+    var z = await transaction.getSigHash(inputIndex: 0);
+    final privateKey = PrivateKey(secret: BigInt.from(8675309));
+    var der = privateKey.sign(z: z).der();
+    var sig = Uint8List.fromList(<int>[
+      ...der,
+      ...ObjectUtils.bigIntToBytes(
+        number: TransactionUtils.hashTypeValue(
+          hashType: HashType.sigHashAll,
+        ),
+        endian: Endian.big,
+        size: 1,
+      ),
+    ]);
+    var sec = privateKey.point.sec();
+    var scriptSig = Script(cmds: [sig, sec]);
+    transaction.txIns[0] = transaction.txIns[0].copyWith(
+      scriptSig: scriptSig,
+    );
+
+    var expected =
+        '010000000199a24308080ab26e6fb65c4eccfadf76749bb5bfa8cb08f291320b3c21e56f0d0d0000006b4830450221008ed46aa2cf12d6d81065bfabe903670165b538f65ee9a3385e6327d80c66d3b502203124f804410527497329ec4715e18558082d489b218677bd029e7fa306a72236012103935581e52c354cd2f484fe8ed83af7a3097005b2f9c60bff71d35bd795f54b67ffffffff02408af701000000001976a914d52ad7ca9b3d096a38e752c2018e6fbc40cdf26f88ac80969800000000001976a914507b27411ccf7f16f10297de6cef3f291623eddf88ac00000000';
+    var hexSerialized = ObjectUtils.bytesToHex(
+      bytes: transaction.serialize(),
+    );
+    print(expected == hexSerialized);
+
+    final rawTx = ObjectUtils.bytesFromHex(
+      hex:
+          '0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600',
+    );
+    transaction = TransactionFactory.parseSync(
+      bytes: rawTx,
+      testnet: false,
+    );
+
+    z = await transaction.getSigHash(inputIndex: 0);
+    der = privateKey.sign(z: z).der();
+    sig = Uint8List.fromList(<int>[
+      ...der,
+      ...ObjectUtils.bigIntToBytes(
+        number: TransactionUtils.hashTypeValue(
+          hashType: HashType.sigHashAll,
+        ),
+        endian: Endian.big,
+        size: 1,
+      ),
+    ]);
+    sec = privateKey.point.sec();
+    scriptSig = Script(cmds: [sig, sec]);
+    transaction.txIns[0] = transaction.txIns[0].copyWith(
+      scriptSig: scriptSig,
+    );
+
+    expected =
+        '0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006a47304402207db2402a3311a3b845b038885e3dd889c08126a8570f26a844e3e4049c482a11022010178cdca4129eacbeab7c44648bf5ac1f9cac217cd609d216ec2ebc8d242c0a012103935581e52c354cd2f484fe8ed83af7a3097005b2f9c60bff71d35bd795f54b67feffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600';
+    hexSerialized = ObjectUtils.bytesToHex(
+      bytes: transaction.serialize(),
+    );
+    print(expected == hexSerialized);
   }
 }
